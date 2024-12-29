2aecc005e897e5c38f8ae8cd49b162e38bcd5dbd
engrron30 <ronaldtorresrosete30@gmail.com>
2024-12-30 05:48:19 +0800

    0.4 Added BeginDrawing, ClearBackground andd EndDrawing functions

--- axe_game.cpp
+++ axe_game.cpp
@@ -6,15 +6,14 @@ int main()
 {
     printf("This is the AXE GAME!\n");
 
-    bool hello{ 2 > 12};
-
-
     int length = 1350;
     int width = 700;
     InitWindow(length, width, "AXE GAME");
 
     while (true) {
-
+        BeginDrawing();
+        ClearBackground(RED);
+        EndDrawing();
     }
     return 0;
 }
