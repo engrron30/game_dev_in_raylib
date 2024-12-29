6c8aafba5e940da4edb61458d69c5f9472d7b665
engrron30 <ronaldtorresrosete30@gmail.com>
2024-12-30 05:41:06 +0800

    0.1 Added InitWindow

---
+++ axe_game.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "raylib.h"
+
+int main()
+{
+    printf("This is the AXE GAME!\n");
+
+    bool hello{ 2 > 12};
+
+
+    int length = 1350;
+    int width = 700;
+    InitWindow(length, width, "AXE GAME");
+
+    while (true) {
+
+    }
+    return 0;
+}
