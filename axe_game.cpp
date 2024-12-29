e97e83d6365ec98874a831ae2a03af9f91c04564
engrron30 <ronaldtorresrosete30@gmail.com>
2024-12-30 06:08:19 +0800

    0.7 Added variables for circle coordinates

--- axe_game.cpp
+++ axe_game.cpp
@@ -6,15 +6,23 @@ int main()
 {
     printf("This is the AXE GAME!\n");
 
+    // Window dimensions
     int length = 1350;
     int width = 700;
     InitWindow(length, width, "AXE GAME");
+
+    // Circle coordinates
+    int circle_x = 175;
+    int circle_y = 100;
 
     while (!WindowShouldClose()) {
         BeginDrawing();                 // we can move and minimize the window around
         ClearBackground(RED);           // adds red background in canvas
 
-        DrawCircle(175, 100, 25, BLUE);
+        // Game logic starts here:
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+        // Game logic ends here!
+
         EndDrawing();
     }
     return 0;
