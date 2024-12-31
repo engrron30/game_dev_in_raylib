6a753bfd4acf4e8f831fee2caeb6fd3fb1853533
engron30 <ronaldtorresrosete30@gmail.com>
2024-12-31 11:13:58 +0800

    1.2 Added the updated edges inside the while and restructured booolean for collision

--- main.c
+++ main.c
@@ -18,11 +18,6 @@ int main()
     int radius   = 25;
     char circle_coor[50];
 
-    int circle_x_right = circle_x + radius;
-    int circle_x_left  = circle_x - radius;
-    int circle_x_top   = circle_y - radius;
-    int circle_y_down  = circle_y + radius;
-
     // Axe coordinates
     int axe_x    = 400;
     int axe_y    = 0;
@@ -36,22 +31,32 @@ int main()
     int text_width_offset  = 100 + 20;
 
     // Game over defines
-    bool collision_with_axe = true;
+    bool collision_with_axe = false;
 
     SetTargetFPS(60);                   // Without this, the circle will just move so fast
     while (!WindowShouldClose()) {
         BeginDrawing();                 // we can move and minimize the window around
         ClearBackground(RAYWHITE);      // adds red background in canvas
 
-        if (collision_with_axe) 
+        if (!collision_with_axe)
         {
-            DrawText("GAME OVER!", window_length / 2 - 150, window_width / 2 - 100, 40, RED);
-        } else {
             // Initialize objects
             DrawCircle(circle_x, circle_y, radius, BLUE);
             DrawRectangle(axe_x, axe_y, axe_len, axe_wid, VIOLET);
+            
+            // Initialize circle edges
+            int circle_x_right = circle_x + radius;
+            int circle_x_left  = circle_x - radius;
+            int circle_y_top   = circle_y - radius;
+            int circle_y_down  = circle_y + radius;
 
-            // Text Logic
+            // Initialize axe edges
+            int axe_x_left     = axe_x;
+            int axe_x_right    = axe_x + axe_len;
+            int axe_y_up       = axe_y;
+            int axe_y_down     = axe_y + axe_len;
+
+            // Print coordinates of circle and axe in lower left
             DrawText("Coordinates:", window_length - text_length_offset - 120, window_width - text_width_offset - 20, 40, BLACK);
             sprintf(circle_coor, "circle:   x = %4d   y = %d", circle_x, circle_y);
             sprintf(axe_coor,    "axe:      x = %4d   y = %d", axe_x,    axe_y);
@@ -59,13 +64,13 @@ int main()
             DrawText(axe_coor,    window_length - text_length_offset, window_width - text_width_offset + 60, 20, DARKGRAY);
 
             // Circle movement
-            if (IsKeyDown(KEY_D) && circle_x_right < window_length - radius) {                   // Always subtract or add with radius to avoid the circle overlapping with borders
+            if (IsKeyDown(KEY_D) && circle_x_right < window_length) {                   // Always subtract or add with radius to avoid the circle overlapping with borders
                 circle_x += 10;
-            } else if (IsKeyDown(KEY_A) && circle_x > radius) {
+            } else if (IsKeyDown(KEY_A) && circle_x_left > 0) {
                 circle_x -= 10;
-            } else if (IsKeyDown(KEY_W) && circle_y > radius + 5) {                      // offset to avoid upper and lower borders of window = 5
+            } else if (IsKeyDown(KEY_W) && circle_y_top > 5) {                      // offset to avoid upper and lower borders of window = 5
                 circle_y -= 10;
-            } else if (IsKeyDown(KEY_S) && circle_y < window_width - radius - 5) {
+            } else if (IsKeyDown(KEY_S) && circle_y_down < window_width - 5) {
                 circle_y += 10;
             }
 
@@ -74,6 +79,9 @@ int main()
                 direction = -direction;
             }
             axe_y += direction;
+
+        } else {
+            DrawText("GAME OVER!", window_length / 2 - 150, window_width / 2 - 100, 40, RED);
         }
 
         // Game logic ends here!
